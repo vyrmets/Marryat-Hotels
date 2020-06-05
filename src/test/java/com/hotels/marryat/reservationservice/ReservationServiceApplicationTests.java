de0e6f0c3c8b0dd41109a6e9856790bdbb930621
Serhii Onyshchenko <serhii.onyshchenko@refinitiv.com>
2020-06-05 14:31:04 +0300

    init spring-boot app

---
+++ src/test/java/com/hotels/marryat/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotels.marryat.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
